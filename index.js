de3ef58f273e76655217ecb13ef65521b7f10254
Yeraldin Espinosa Shaik <130239255+YeralShaik@users.noreply.github.com>
2023-09-18 17:46:22 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,11 +2,7 @@
 const quotebutton = document.getElementById ('quote-button');
 const instagram = document.getElementById ('instagram');
 const mail= document.getElementById ('mail');
-<<<<<<< HEAD
 
-=======
-const whatsapp = document.getElementById ('Whatsapp');
->>>>>>> 1f1b16b9305601be3cf638d0da53bb009abea548
 
 // controlador de eventos al botón para el evento de clic
 quotebutton.addEventListener('click', function() {
